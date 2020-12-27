b382d6015c5557f956c8131e828731c05d3b3cd4
Benjamin Christau <benjamin.christau@oyez.fr>
2020-12-27 19:53:52 +0100

    [TEST] Github Action - Terraform first commit

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,5 +1,15 @@
 terraform {
   required_version = ">= 0.13"
+
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "Timeline"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "timeline-iaas"
+    }
+  }
 }
 
 provider "google" {
